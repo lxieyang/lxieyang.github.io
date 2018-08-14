db249f350f0c0fcb39a7fcc75d39878639b1d7b8
lxieyang <lxieyang@umich.edu>
2018-08-14 00:10:54 -0400

    try to pass build

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -20,16 +20,19 @@ class Header extends Component {
     this.toggle = this.toggleNavbar.bind(this);
 
     // https://www.w3schools.com/howto/howto_js_navbar_hide_scroll.asp
-    var prevScrollpos = window.pageYOffset;
-    window.onscroll = function () {
-    var currentScrollPos = window.pageYOffset;
-      if (prevScrollpos > currentScrollPos) {
-        document.getElementById("navbar").style.top = "0";
-      } else {
-        document.getElementById("navbar").style.top = "-5.0rem";
+    if (typeof window !== 'undefined') {
+      var prevScrollpos = window.pageYOffset;
+      window.onscroll = function () {
+      var currentScrollPos = window.pageYOffset;
+        if (prevScrollpos > currentScrollPos) {
+          document.getElementById("navbar").style.top = "0";
+        } else {
+          document.getElementById("navbar").style.top = "-5.0rem";
+        }
+        prevScrollpos = currentScrollPos;
       }
-      prevScrollpos = currentScrollPos;
     }
+    
   }
 
   state = {
