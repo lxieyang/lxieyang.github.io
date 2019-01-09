c092c274f840c20ecc288931d3dfda9902be1be5
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-01-09 05:54:09 +0000

    Deploy lxieyang/lxieyang.github.io to github.com/lxieyang/lxieyang.github.io.git:master

--- sw.js
+++ sw.js
@@ -26,7 +26,7 @@ workbox.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-ff35312bc08432c45cba.js"
+    "url": "webpack-runtime-0cee270332eb8e53c17c.js"
   },
   {
     "url": "app-a31d206d7bbe8b515d89.js"
@@ -36,7 +36,7 @@ self.__precacheManifest = [
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "c3c7d3a2d09fcfb94879fb73b8a11300"
+    "revision": "31e7f47b2b0bfb32ef2fd2818b81ce4e"
   },
   {
     "url": "0.08eb512c534233bffd38.css"
