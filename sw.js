fc463f9702f9b0b8c51726335ad1f0821076a695
lxieyang <lxieyang@umich.edu>
2023-04-12 13:54:50 -0400

    Updates

--- sw.js
+++ sw.js
@@ -27,7 +27,7 @@ workbox.core.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-2d9aa87048fcd7caea10.js"
+    "url": "webpack-runtime-2cbddae4afd2d3555825.js"
   },
   {
     "url": "framework-13ae926ae6f806f6d5ee.js"
@@ -37,7 +37,7 @@ self.__precacheManifest = [
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "98ab400ead9a9c9207407a2620c4b2bd"
+    "revision": "2e9bd104b9434362a2d990bb54aa1ab0"
   },
   {
     "url": "polyfill-7d7eacf769b09c482a01.js"
