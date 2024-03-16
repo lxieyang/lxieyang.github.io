d8978221db2c2568bc71d201573a4e4d877699bd
Michael Xieyang Liu <lxieyang@umich.edu>
2024-03-15 20:26:46 -0400

    Updates

--- sw.js
+++ sw.js
@@ -27,17 +27,17 @@ workbox.core.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-25f35b9501123eb1ab20.js"
+    "url": "webpack-runtime-6990cf6e74cf07ec7380.js"
   },
   {
     "url": "framework-fd985e6a7cdfcbc24db4.js"
   },
   {
-    "url": "app-238c6eb3b6cf8a83d84b.js"
+    "url": "app-2b9bd7c2a6647458233b.js"
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "5cf638d72f68ccb41d04bdeacf7e1b88"
+    "revision": "0fa0ae8c3009251ede8e826e06af6612"
   },
   {
     "url": "manifest.webmanifest",
@@ -151,7 +151,7 @@ const navigationRoute = new NavigationRoute(async ({ event }) => {
   // Check for resources + the app bundle
   // The latter may not exist if the SW is updating to a new version
   const resources = await idbKeyval.get(`resources:${pathname}`)
-  if (!resources || !(await caches.match(`/app-238c6eb3b6cf8a83d84b.js`))) {
+  if (!resources || !(await caches.match(`/app-2b9bd7c2a6647458233b.js`))) {
     return await fetch(event.request)
   }
 
