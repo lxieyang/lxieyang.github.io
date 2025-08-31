8c9981197e730c8537b130c010469f3e8a6f0896
Michael Xieyang Liu <lxieyang@umich.edu>
2025-08-31 19:54:10 -0400

    migrate StaticQuery to useStaticQuery

--- src/components/layout/layout.jsx
+++ src/components/layout/layout.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { StaticQuery, graphql } from 'gatsby';
+import { graphql, useStaticQuery } from 'gatsby';
 import { Container } from 'reactstrap';
 
 import Header from '../header/header';
@@ -9,45 +9,39 @@ import './bootstrap.min.css';
 import './layout.css';
 require('prismjs/themes/prism-tomorrow.css');
 
-const Layout = ({ children, data, pathName }) => {
-  return (
-    <StaticQuery
-      query={graphql`
-        query SiteTitleQuery {
-          site {
-            siteMetadata {
-              title
-              externalLinks {
-                googleScholarProfile
-                github
-                facebook
-                instagram
-                twitter
-              }
-            }
+const Layout = ({ children, pathName }) => {
+  const data = useStaticQuery(graphql`
+    query SiteTitleQuery {
+      site {
+        siteMetadata {
+          title
+          externalLinks {
+            googleScholarProfile
+            github
+            facebook
+            instagram
+            twitter
           }
         }
-      `}
-      render={(data) => (
-        <div>
-          <Header
-            siteTitle={data.site.siteMetadata.title}
-            pathName={pathName}
-          />
-          <Container
-            style={{
-              paddingTop: '5.0rem',
-            }}
-          >
-            {children}
-            <Footer
-              siteTitle={data.site.siteMetadata.title}
-              links={data.site.siteMetadata.externalLinks}
-            />
-          </Container>
-        </div>
-      )}
-    />
+      }
+    }
+  `);
+
+  return (
+    <div>
+      <Header siteTitle={data.site.siteMetadata.title} pathName={pathName} />
+      <Container
+        style={{
+          paddingTop: '5.0rem',
+        }}
+      >
+        {children}
+        <Footer
+          siteTitle={data.site.siteMetadata.title}
+          links={data.site.siteMetadata.externalLinks}
+        />
+      </Container>
+    </div>
   );
 };
 
